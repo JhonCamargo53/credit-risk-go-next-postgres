67da7c15b8f95b6946ad3cdf3e5501bee025e20f
Jhon Camargo <jcamargocelin@gmail.com>
2025-12-01 00:23:35 -0500

    feat: update project name and metadata for Credit Risk Manager

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -6,10 +6,43 @@ import LayoutWithAuth from "./layout-with-auth";
 import { ModalProvider } from "@/context/ModalContext";
 
 export const metadata: Metadata = {
-  title: "Product Manager",
-  description: "Gestor de rifas",
+  title: "Credit Risk Manager – MVP",
+  description:
+    "MVP del sistema inteligente para gestión de solicitudes de crédito, evaluación automática de riesgo, generación de reportes financieros y explicación en lenguaje natural.",
+  keywords: [
+    "credit risk",
+    "evaluación de riesgo",
+    "MVP",
+    "gestor de crédito",
+    "finanzas",
+    "score crediticio",
+    "solicitudes de crédito",
+    "análisis financiero",
+    "reportes automáticos",
+    "machine learning",
+    "Golang backend",
+    "Next.js frontend",
+    "PostgreSQL",
+  ],
+  authors: [
+    { name: "Jhon Camargo" },
+  ],
+  applicationName: "Credit Risk Manager",
+  category: "Finanzas / Análisis de Riesgo",
+  generator: "Next.js",
+  creator: "Jhon Camargo",
+  publisher: " Prueba Tecnica -Ingeniería de Sistemas",
+  robots: "index, follow",
+  openGraph: {
+    title: "Credit Risk Manager – MVP",
+    description:
+      "Sistema MVP para evaluar riesgo crediticio, gestionar solicitudes y generar reportes financieros automatizados.",
+    url: "https://tu-dominio.com",
+    siteName: "Credit Risk Manager",
+    locale: "es_CO",
+    type: "website",
+  }
 };
-
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
